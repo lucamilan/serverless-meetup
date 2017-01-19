16232a779a68c3d14a392899f22b999b8fa380ad
Luca Milan <luca.milan@gmail.com>
2017-01-19 10:00:54 +0100

    sample01 completed.

--- sample01/handler.js
+++ sample01/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = (event, context, callback) => {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      input: event
     })
   };
 
--- sample01/help.md
+++ sample01/help.md
@@ -1,5 +1,11 @@
-# help #
+# help sls #
 
 sls deploy -s dev
 sls invoke -f hello
-sls logs -f hello
+sls logs -f hello [-t tail mode]
+sls info -s dev
+
+# help npm #
+
+npm init
+npm list -g aws-sdk
