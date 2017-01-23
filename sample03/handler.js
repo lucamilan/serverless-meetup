bd86408e724a5401635820983679757ecc956aa4
Luca Milan <luca.milan@gmail.com>
2017-01-23 09:20:35 +0100

    better names and try.catch.

--- sample03/handler.js
+++ sample03/handler.js
@@ -1,11 +1,16 @@
-import Builder from './builder';
+import Concatenator from './concatenator';
 
 export const hello = async(event, context, callback) => {
-  const builder = new Builder();
-  const part1 = await builder.build('Hello', 'Wolf');
-  const part2 = await builder.build('Welcome', 'Serverless');
-  
-  callback(null, {
-    message: await builder.build(part1, 'and', part2)
-  });
+  const concat = new Concatenator();
+  let message = '';
+
+  try {    
+    const part1 = await concat.delayed('Hello', 'Wolf');
+    const part2 = await concat.delayed('Welcome', 'Serverless');
+    message = await concat.immediate(part1, 'and', part2);
+  } catch (error) {
+    message = 'Okay, Houston, we\'ve had a problem here!';
+  }
+
+  callback(null, message);
 };
